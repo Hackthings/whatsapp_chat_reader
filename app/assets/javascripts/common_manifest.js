b9fc4801ad1e5e7cf6e5ff178136d7c716e734bc
Vishal Nagda <vishalnagda1@gmail.com>
2017-02-19 01:14:31 +0530

    create basic UI, seprate common and home files in assets.

--- app/assets/javascripts/common_manifest.js
+++ app/assets/javascripts/common_manifest.js
@@ -1,2 +1,4 @@
 //= require common/jquery.min.js
 //= require common/bootstrap.min.js
+
+//= require home/load_chat.js
---
+++ app/assets/javascripts/home/load_chat.js
@@ -0,0 +1,10 @@
+var WCR = WCR || {};
+
+WCR.LoadChat = function () {
+  this.initialize();
+}
+WCR.LoadChat.prototype = {
+  initialize:function () {
+    console.log("Success...");
+  }
+}
